9838d1ed7cb91be9578f58bc95384faa8581f79a
ernimish13 <ernimish13@gmail.com>
2023-10-10 23:10:23 +0530

    Update RqChallengeApplicationTests.java

--- src/test/java/com/example/rqchallenge/RqChallengeApplicationTests.java
+++ src/test/java/com/example/rqchallenge/RqChallengeApplicationTests.java
@@ -5,9 +5,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RqChallengeApplicationTests {
 
-//    @Test
-//    void check() {
-//    	System.out.println();
-//    }
-
 }
